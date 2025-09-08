2e3e396e8703879ac09e4bf5877d7d575c035ca3
Ajay Reddy <you@example.com>
2025-09-08 12:01:40 +0530

    made changes in views and index.html and added base.html

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import TrackingHistory, CurrentBalance
+from django.db.models import Sum
+from django.contrib import messages
 
 # Create your views here.
 
@@ -13,6 +15,10 @@ def index(request):
         expense_type = "CREDIT"
         if float(amount) < 0:
             expense_type = "DEBIT"
+        
+        if float(amount) == 0:
+            messages.success(request,'amount cannot be zero')
+            return redirect('/')
         tracking_history = TrackingHistory.objects.create(
             amount = amount,
             expense_type = expense_type,
@@ -21,6 +27,28 @@ def index(request):
         )
         current_balance.current_balance += float(tracking_history.amount)
         current_balance.save()
-        print(description, amount)
+        # print(description, amount)
         return redirect('/')
-    return render(request, 'index.html')
+    current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+    income = 0
+    expense = 0
+    for tracking_history  in TrackingHistory.objects.all():
+        if tracking_history.expense_type == 'CREDIT':
+            income += tracking_history.amount
+        else:
+            expense += tracking_history.amount
+    context = {'income' : income ,'expense' : expense ,'transactions' : TrackingHistory.objects.all(), 'current_balance' : current_balance}
+    return render(request, 'index.html', context)
+
+
+
+
+def delete_transaction(request, id):
+   tracking_history = TrackingHistory.objects.filter(id =id)
+   if tracking_history.exists():
+       current_balance, _ = CurrentBalance.objects.get_or_create(id=1)
+       tracking_history = tracking_history[0]
+       current_balance.current_balance = current_balance.current_balance - tracking_history.amount
+       current_balance.save()
+   tracking_history.delete()
+   return redirect('/')
